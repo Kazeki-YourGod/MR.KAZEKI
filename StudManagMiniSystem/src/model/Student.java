af662fc4c0ecaebb5215251862f4cfdf21336544
Tugsbilegt <Togsbilegt88@gmail.com>
2026-03-23 08:48:47 +0800

    Lab 7 -OOP GUI system

---
+++ StudManagMiniSystem/src/model/Student.java
@@ -0,0 +1,16 @@
+package model;
+
+public class Student extends Person {
+	private double gpa;
+	
+	public double getGpa() {
+		return gpa;
+	}
+	public void setGpa(double gpa) {
+		this.gpa = gpa ;
+	}
+	@Override
+	public String toString() {
+		return name + "|GPA"+ gpa;
+	}
+}
